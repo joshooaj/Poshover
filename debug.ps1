fad32e6ff483f088e17860ff10fa6025c7be6962
Josh Hendricks <jh@milestone.dk>
2021-05-24 13:30:28 -0700

    Add note to debug.ps1 launch script

--- debug.ps1
+++ debug.ps1
@@ -1,3 +1,12 @@
+<#
+    Used as a launch script in VSCode to...
+    - Remove all previous builds sitting in the output folder
+    - Build the module using the psakefile.ps1 build task
+    - Find the module manifest and force import it
+
+    This makes for a quick developer "inner loop"
+#>
+
 Get-ChildItem $PSScriptRoot\output -ErrorAction Ignore | Remove-Item -Recurse -Force
 Invoke-psake build
 $manifest = Get-ChildItem -Path $PSScriptRoot\output\*.psd1 -Recurse
