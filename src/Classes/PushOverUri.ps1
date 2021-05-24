98272c8dee2db6e3f32bf4b247b02b7a673660d4
Josh Hendricks <jh@milestone.dk>
2021-05-24 15:09:11 -0700

    Fix broken URI class members

--- src/Classes/PushOverUri.ps1
+++ src/Classes/PushOverUri.ps1
@@ -1,7 +1,8 @@
-class PushOverUri {
-    hidden [string]$BaseUri = 'https://api.pushover.net/1'
-    static [uri]$Messages = $BaseUri + '/messages.json'
+class PushoverUri {
+    hidden static [string]$BaseUri = 'https://api.pushover.net/1'
+
+    static [uri]$Messages = [PushoverUri]::BaseUri + '/messages.json'
 
     # See https://pushover.net/api/receipts for details. You must add /(your-receipt).json?token=(your token) or /(your-receipt)/cancel.json or /cancel_by_tag/(your-tag).json
-    static [uri]$Receipts = $BaseUri + '/receipts'
+    static [uri]$Receipts = [PushoverUri]::BaseUri + '/receipts'
 }
